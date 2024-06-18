e1d5c981184f923178e3e7b875bd2d0cfb743ccd
unknown <cdsergiu@outlook.com>
2024-06-18 19:54:12 +0300

    Updating cadnumber-validation project

--- cardnumber-validation/Program.cs
+++ cardnumber-validation/Program.cs
@@ -6,38 +6,36 @@ Console.WriteLine(ValidateCardNumber(creditCardNumber));
 
 static bool ValidateCardNumber(string input)
 {
-    //Converting the string into an int array
-    int[] cardDigits = new int[CreditCardLength];
-
-    for (int i = 0; i < CreditCardLength; i++)
+    if(string.IsNullOrWhiteSpace(input) || input.Length != CreditCardLength)
     {
-        cardDigits[i] = input[i] - '0';
-    }    
+        return false;
+    }
 
     int sum = 0;
 
     //Using the Luhn Algorithm we are multiplying the even possitions by 2.
-    //If the sum is greater than 9, we are adding the digits together
+    //If the sum is greater than 9, we are adding the digits together.
     for (int i = 0; i < CreditCardLength; i++)
     {
+        int currentDigit = input[i] - '0';
+
         if (i % 2 == 0)
         {
-            if(cardDigits[i] * 2 > 9)
-            {
-                //Because the maximum sum we can get is 9*2 = 18. We can add 1 and the last digit.
-                //Since the first digit will be always 1.
-                sum += ((cardDigits[i] * 2) % 10) + 1;
+            if(currentDigit * 2 > 9)
+            {  
+                sum += ((currentDigit * 2) % 10) + 1;
             }
             else
             {
-                sum += cardDigits[i] * 2;
+                sum += currentDigit * 2;
             }
         }
         else
         {
-            sum += cardDigits[i];
+            sum += currentDigit;
         }
     }
-    Console.WriteLine("Sum of the processed digits: " + sum);
+
+    //If the sum of the processed digits is divisible by 10, the card number is valid.
     return sum % 10 == 0;
 }
