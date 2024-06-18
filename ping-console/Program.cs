0d84f4932d2a21ac7a6d49e848a5175b447369cb
unknown <cdsergiu@outlook.com>
2024-06-18 13:59:20 +0300

    Adding ping-console back

---
+++ ping-console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+
+Console.WriteLine(PingAddress("www.google.com", 1000));
+static string PingAddress(string address, int timeout)
+{
+
+    try
+    {
+        Ping ping = new Ping();
+        PingReply reply= ping.Send(address, timeout);
+        return "Status: " + reply.Status.ToString() + "; Time: " + reply.RoundtripTime + "ms ;";
+    }
+    catch (Exception e)
+    {
+        return e.Message + " " + e.InnerException.Message;
+    }
+    
+    
+}
