49f23c91a396206892b96234b9270f07cc5677c6
unknown <cdsergiu@outlook.com>
2024-06-18 14:27:04 +0300

    Adding ip-validation project

---
+++ ip-validation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+string input = "10.8.244.01";
+
+Console.WriteLine(ValidateIP(input));
+
+static bool ValidateIP(string input)
+{
+    IPAddress address;    
+
+    return IPAddress.TryParse(input, out address) && address.ToString() == input;
+}
