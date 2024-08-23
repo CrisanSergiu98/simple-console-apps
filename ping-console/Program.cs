e6e792f5aa09686c1a6dd4deb3f7dc47f4084840
unknown <cdsergiu@outlook.com>
2024-08-23 18:20:11 +0300

    update

--- ping-console/Program.cs
+++ ping-console/Program.cs
@@ -1,19 +1,48 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Net;
+using System.Net.NetworkInformation;
 
-Console.WriteLine(PingAddress("www.google.com", 1000));
+string ipInput = "192.168.56";
+
+for(int i = 1; i < 255; i++)
+{
+    string ip = ipInput + "." + i;
+    Console.WriteLine(PingAddress(ip, 1000));
+}
+
 static string PingAddress(string address, int timeout)
 {
-
     try
     {
         Ping ping = new Ping();
-        PingReply reply= ping.Send(address, timeout);
-        return "Status: " + reply.Status.ToString() + "; Time: " + reply.RoundtripTime + "ms ;";
+
+        IPAddress ip;
+        IPHostEntry entry;
+
+        IPAddress.TryParse(address, out ip);
+
+        if (ip == null)
+        {
+            return "Incorrect IP Address";
+        }
+
+        PingReply reply = ping.Send(ip, timeout);
+
+        string hostOutputText = "";
+
+        if (reply.Status != IPStatus.Success)
+        {
+            hostOutputText = "No Host Found";
+        }
+        else
+        {
+            entry = Dns.GetHostEntry(ip);
+            hostOutputText = entry.HostName;
+        }
+
+        return "Host: " + hostOutputText + "; Status: " + reply.Status.ToString() + "; Time: " + reply.RoundtripTime + "ms ;";
     }
     catch (Exception e)
     {
         return e.Message + " " + e.InnerException.Message;
-    }
-    
-    
+    }  
 }
